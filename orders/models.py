e26177ac9094f86e300f833e09720a12c8f0b194
prathikshaa.selva <prathikshaa.selva@aspiresys.com>
2024-05-14 21:01:22 +0530

    14-05-2024

--- orders/models.py
+++ orders/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from users.models import CustomUser
 from products.models import Product
 
-# Create your models here.
 class Order(models.Model):
+    STATUS_CHOICES = (
+        ('in_transit', 'In Transit'),
+        ('delivered', 'Delivered')
+    )
+    
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
-    product_name= models.ForeignKey(Product, on_delete=models.CASCADE,db_column="product_id")
+    product_name = models.ForeignKey(Product, on_delete=models.CASCADE, db_column="product_id")
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='in_transit')
+
+    def __str__(self):
+        return f"Order {self.pk} - {self.status}"
